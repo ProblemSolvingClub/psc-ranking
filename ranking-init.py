fa0a047398b3469eddf329547cc774f676ca480e
Jan Clarin <hi@janclarin.com>
2016-09-16 00:17:15 -0600

    add seed data for tiers and sites

--- ranking-init.py
+++ ranking-init.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+'''
+Creates database tables and populates them with seed data.
+'''
 import collections, os, sqlite3
 
 # This assumes that ranking.sqlite3 is in the same folder as this script.
@@ -46,36 +49,55 @@ def init_site_score():
                'created_date DATETIME DEFAULT CURRENT_TIMESTAMP, '
                'PRIMARY KEY(user_id, site_id))')
 
+def seed_tier():
+    Tier = collections.namedtuple('Tier', 'id name minimum_score')
+    tiers = [
+                Tier(id=1, name='Bronze', minimum_score=0),
+                Tier(id=2, name='Silver', minimum_score=5),
+                Tier(id=3, name='Gold', minimum_score=15),
+                Tier(id=4, name='Platinum', minimum_score=30),
+                Tier(id=5, name='Diamond', minimum_score=40)
+            ]
+
+    for tier in tiers:
+        db.execute('INSERT INTO tier (id, name, minimum_score) VALUES (?,?,?)',
+                (tier.id, tier.name, tier.minimum_score))
+
 def seed_site():
     Site = collections.namedtuple('Site', 'id name profile_url')
     sites = [
-        Site(id=1, name='Caribbean Online Judge',
-            profile_url='http://coj.uci.cu/user/useraccount.xhtml?username=%s'),
-        Site(id=2, name='CodeChef', 
-            profile_url='https://www.codechef.com/users/%s'),
-        Site(id=3, name='Codeforces',
-            profile_url='http://www.codeforces.com/api/user.status?handle=%s'),
-        Site(id=4, name='ICPC Live Archive',
-            profile_url='https://open.kattis.com/users/%s'),
-        Site(id=5, name='Kattis',
-            profile_url='https://open.kattis.com/users/%s'),
-        Site(id=6, name='Peking Online Judge',
-            profile_url='http://poj.org/userstatus?user_id=%s'),
-        Site(id=7, name='Sphere Online Judge',
-            profile_url='http://www.spoj.com/users/%s/'),
-        Site(id=8, name='UVa Online Judge', 
-            profile_url='https://icpcarchive.ecs.baylor.edu/uhunt/api/solved-bits/%s'),
-    ]
+            Site(id=1, name='Caribbean Online Judge',
+                profile_url='http://coj.uci.cu/user/useraccount.xhtml?username=%s'),
+            Site(id=2, name='CodeChef', 
+                profile_url='https://www.codechef.com/users/%s'),
+            Site(id=3, name='Codeforces',
+                profile_url='http://www.codeforces.com/api/user.status?handle=%s'),
+            Site(id=4, name='ICPC Live Archive',
+                profile_url='https://open.kattis.com/users/%s'),
+            Site(id=5, name='Kattis',
+                profile_url='https://open.kattis.com/users/%s'),
+            Site(id=6, name='Peking Online Judge',
+                profile_url='http://poj.org/userstatus?user_id=%s'),
+            Site(id=7, name='Sphere Online Judge',
+                profile_url='http://www.spoj.com/users/%s/'),
+            Site(id=8, name='UVa Online Judge', 
+                profile_url='https://icpcarchive.ecs.baylor.edu/uhunt/api/solved-bits/%s'),
+            ]
 
     for site in sites:
         db.execute('INSERT INTO site (id, name, profile_url) VALUES (?,?,?)',
                 (site.id, site.name, site.profile_url))
 
+# Create tables if they don't exist.
 init_tier()
 init_user()
 init_site()
 init_site_account()
 init_site_score()
+
+# Seed the tables with initial data.
+seed_tier()
 seed_site()
 
+# Commit database transactions.
 db.commit()
